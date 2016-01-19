c94e025fa477118a74fd1cfa9dc014fd95c69a32
Wex <jtgowan@yahoo.com>
2016-01-19 01:40:08 -0500

    Working Menu Start and Exit buttons
    Need to add function to scene select

---
+++ ProjectLain/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace StartMenu
+{
+	
+public class LevelManager : MonoBehaviour {
+
+	public void LoadScene(string name)
+	{
+		Application.LoadLevel (name);
+	}
+
+
+//		need to set name property
+//		public void SelectScene(string name)
+//		{
+//			Application.
+//		}
+
+
+	public void ExitGame()
+	{
+		Application.Quit ();
+	}
+
+}
+}
