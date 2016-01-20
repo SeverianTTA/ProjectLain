014a68fe42559cf5f9d32bb60202e8de675e892b
Wex <jtgowan@yahoo.com>
2016-01-20 14:10:20 -0500

    Scrapped menu added game manager
    Use num pad keys to navigate scenes.
    Escape key to exit
    Will work on menu later

---
+++ ProjectLain/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class GameManagerScript : MonoBehaviour {
+
+	static GameManagerScript Instance;
+
+	// Use this for initialization
+	void Start () {
+		if (Instance != null) {
+			GameObject.Destroy (gameObject);
+		} else 
+		{
+			GameObject.DontDestroyOnLoad (gameObject);
+			Instance = this;
+		}
+	
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyUp (KeyCode.Keypad0)) 
+		{
+			SceneManager.LoadScene (0);
+		}
+	
+		if (Input.GetKeyUp (KeyCode.Keypad1)) 
+		{
+			SceneManager.LoadScene (1);
+		}
+	
+		if (Input.GetKeyUp (KeyCode.Keypad2)) 
+		{
+			SceneManager.LoadScene (2);
+		}
+	
+		if (Input.GetKeyUp (KeyCode.Escape)) 
+		{
+			Application.Quit();
+		}
+	}
+}
